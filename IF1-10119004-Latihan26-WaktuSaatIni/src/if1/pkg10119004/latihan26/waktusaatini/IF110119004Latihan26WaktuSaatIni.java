ca816ff3751962fcaf0b8e99f0e96e86f098b79e
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-30 23:25:42 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan26-WaktuSaatIni/src/if1/pkg10119004/latihan26/waktusaatini/IF110119004Latihan26WaktuSaatIni.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan26.waktusaatini;
+import java.util.Date;    
+
+/**
+ *
+ * @author ENDOG
+ * Nama     : Muhamad Ramadan
+ * Nim      : 10119004
+ * Kelas    : IF1
+ */
+public class IF110119004Latihan26WaktuSaatIni {
+
+    /**
+     * @param args the command line arguments
+     */
+     public class TestTanggal { 
+     }
+    public static void main(String Args[]){  
+
+       //  object tanggal
+       Date date = new Date();
+        
+       // menampilkan waktu dan tanggal menggunakan toString()
+       System.out.println("Hari ini adalah hari : "+date.toString());
+   }
+}  
